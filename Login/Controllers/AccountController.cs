2a8126036e874cc705e18b49327531142bf92462
semihbyk <semihbaykara2005@gmail.com>
2023-08-08 14:25:35 +0300

    Proje dosyası ekle.

---
+++ Login/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Hesap
+        public ActionResult Login()
+        {
+            return View("Account/Login");
+        }
+        [HttpPost]
+
+        public ActionResult Login(string kadi, string sifre)
+        {
+            if (kadi == "semih" && sifre =="baykara")
+            {
+                return RedirectToAction("Merhaba", "Anasayfa");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Bilgilerinizi Kontrol Ediniz.";
+                return View();
+            }
+
+            
+
+        }
+    }
+}
